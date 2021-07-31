4b02116909aff7cac9bb38bf2c7c4c0ec12fc2e2
CHR-onicles <tpandivine48@gmail.com>
2021-07-31 19:45:35 +0000

    more progress

--- src/script.js
+++ src/script.js
@@ -1,5 +1,10 @@
 const share_icon = document.querySelector(".share-icon");
+const share_icon_active = document.querySelector(".share-icon-active");
+const share_container = document.querySelector(".share-container");
 
-share_icon.addEventListener("click", (e) => {
-    console.log(e.currentTarget);
-})
+share_icon.addEventListener("click", () => {
+    share_container.classList.toggle("active");
+});
+share_icon_active.addEventListener("click", () => {
+    share_container.classList.toggle("active");
+});
