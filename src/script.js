0fdbcee10d4d8696239634926ae242bb962884ee
CHR-onicles <tpandivine48@gmail.com>
2021-08-01 21:29:19 +0000

    refactoring js

--- src/script.js
+++ src/script.js
@@ -1,10 +1,5 @@
-const share_icon = document.querySelector(".share-icon");
-const share_icon_active = document.querySelector(".share-icon-active");
 const share_container = document.querySelector(".share-container");
-
-share_icon.addEventListener("click", () => {
-    share_container.classList.toggle("active");
-});
-share_icon_active.addEventListener("click", () => {
-    share_container.classList.toggle("active");
+const share_btns = [document.querySelector(".share-icon"), document.querySelector(".share-icon-active")];
+share_btns.forEach((btn) => {
+    btn.addEventListener("click", () => share_container.classList.toggle("active"));
 });
